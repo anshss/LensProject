9c3a7c79a690a30b8ac24b3a16b8121b3a6d6a76
Ansh Saxena <anshsaxena4190@gmail.com>
2022-12-17 02:10:11 +0530

    index and profile/index page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    // domains: [
+    //   'ipfs.infura.io',
+    //   'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+    //   'statics-mumbai-lens-staging.s3.eu-west-1.amazonaws.com',
+    //   'lens.infura-ipfs.io',
+    //   ''
+    // ],
+  }, 
 }
 
 module.exports = nextConfig
---
+++ pages/profile/[id].js
@@ -0,0 +1,53 @@
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
+import { client, getProfileById } from '../../api'
+
+export default  function Profile() {
+
+    const [ profile, setProfile ] = useState()
+    
+    const router = useRouter()
+    const { id } = router.query;
+
+    useEffect(()=> {
+        if (id) {
+            fetchProfile()
+        }
+    }, [id])
+
+    async function fetchProfile() {
+        try {
+            const response = await client.query(getProfileById, { id }).toPromise();
+            setProfile(response.data.profiles.items[0])
+            // console.log(response.data.profiles.items[0])
+
+            const publicationData = await client.query(getProfile, { 
+                request: {
+                    id
+                }
+             }).toPromise();
+             console.log( {publicationData})
+             
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    if (!profile) return null
+    return (
+        <div>
+            {
+             profile.picture ? 
+                ( <img src={profile.picture.original.url} width="200px" alt="" /> ) 
+                : ( <div style={{ width: '200px', height: '200px' }} /> )
+            }
+            <div>
+                <h4>{profile.handle}</h4>
+                <p>{profile.bio}</p>
+                <p>Followers: {profile.stats.totalFollowers}</p>
+                <p>Following: {profile.stats.totalFollowing}</p>
+            </div>
+        </div>
+    )
+}
